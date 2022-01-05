d0368bee2a00da6a6cb10c79bc86411632498b48
Sili-Dev <silvan.heini.dev@gmail.com>
2022-01-05 10:33:44 +0100

    add images / add routes

---
+++ backend/_service/product.service.ts
@@ -0,0 +1,77 @@
+import { Product } from "../_model/product.ts";
+
+export class ProductService {
+    db: Product[] = [
+        {
+            id: 1,
+            name: 'Appenzeller',
+            price: 20,
+            directory: '../assets/appenzeller.jpg',
+        },
+        {
+            id: 2,
+            name: 'Eier',
+            price: 20,
+            directory: '../assets/eier.jpg'
+        },
+        {
+            id: 3,
+            name: 'Ice Tea',
+            price: 20,
+            directory: '../assets/icedtea.jpg'
+        },
+        {
+            id: 4,
+            name: 'Kalbswurst',
+            price: 20,
+            directory: '../assets/kalbsbratwuerste.jpg'
+        },
+        {
+            id: 5,
+            name: 'Krustenkranz',
+            price: 20,
+            directory: '../assets/krustenkranz.jpg'
+        },
+        {
+            id: 6,
+            name: 'Nektarinen',
+            price: 20,
+            directory: '../assets/nektarinen.jpg'
+        },
+        {
+            id: 7,
+            name: 'Olivenöl',
+            price: 20,
+            directory: '../assets/olivenoel.jpg'
+        },
+        {
+            id: 8,
+            name: 'Senf',
+            price: 20,
+            directory: '../assets/senf.jpg'
+        },
+        {
+            id: 9,
+            name: 'Tomaten',
+            price: 20,
+            directory: '../assets/tomaten.jpg'
+        },
+        {
+            id: 10,
+            name: 'Vaniell Glace',
+            price: 20,
+            directory: '../assets/vanille_glace.jpg'
+        },
+    ];
+
+    getProducts() {
+        return this.db;
+    }
+
+    getProductById(id: number) {
+        for (let product of this.db) {
+            if (product.id == id) return product;
+        }
+        return null;
+    }
+}
--- backend/router.ts
+++ backend/router.ts
@@ -1,9 +1,23 @@
 import {Router} from 'https://deno.land/x/oak/mod.ts';
+import { ProductService } from "./_service/product.service.ts";
 
 const router = new Router();
 
+const productService = new ProductService();
+
 router.get('/', async (ctx) => {
     
+});
+
+router.get('/products', ctx => {
+    ctx.response.headers.set('Content-Type', 'application/json');
+    ctx.response.body = productService.getProducts();
+});
+
+router.get('/products/:id', ctx => {
+    const id: number = Number.parseInt(ctx.params.id!);
+    ctx.response.headers.set('Content-Type', 'application/json');
+    ctx.response.body = productService.getProductById(id);
 });
 
 export default router;

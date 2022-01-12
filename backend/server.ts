b647d58c8823c30a228a1b14aa55d24c21b7bea8
kota schnider <kota_schnider@sluz.ch>
2022-01-12 09:29:40 +0100

    add oakCors

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,10 @@
 import {Application} from 'https://deno.land/x/oak/mod.ts';
 import router from './router.ts';
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
+
 
 const app = new Application();
+app.use(oakCors({ origin: "*" }));
 app.use(router.routes());
 app.use(router.allowedMethods());
 console.log('Server is listening on 8080');
